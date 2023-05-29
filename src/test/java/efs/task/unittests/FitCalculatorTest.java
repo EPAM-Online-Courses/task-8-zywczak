2388f7362ecf62febcf15a63069eba90484928bd
zywczak <123404938+zywczak@users.noreply.github.com>
2023-05-29 18:18:18 +0200

    Update FitCalculatorTest.java

--- src/test/java/efs/task/unittests/FitCalculatorTest.java
+++ src/test/java/efs/task/unittests/FitCalculatorTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class FitCalculatorTest {
 
