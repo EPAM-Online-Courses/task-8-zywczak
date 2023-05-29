4bcd3db161fe5533d2dccf491b8bed48f47653dd
zywczak <123404938+zywczak@users.noreply.github.com>
2023-05-29 19:21:20 +0200

    Update PlannerTest.java

--- src/test/java/efs/task/unittests/PlannerTest.java
+++ src/test/java/efs/task/unittests/PlannerTest.java
@@ -42,9 +42,9 @@ class PlannerTest {
 
         // then
         DailyIntake expectedDailyIntake = TestConstants.TEST_USER_DAILY_INTAKE;
-        assertEquals(dailyIntake.getCalories(), expectedDailyIntake.getCalories());
-        assertEquals(dailyIntake.getProtein(), expectedDailyIntake.getProtein());
-        assertEquals(dailyIntake.getFat(), expectedDailyIntake.getFat());
-        assertEquals(dailyIntake.getCarbohydrate(), expectedDailyIntake.getCarbohydrate());
+        assertEquals(dailyIntake, expectedDailyIntake);
+        //assertEquals(dailyIntake.getProtein(), expectedDailyIntake.getProtein());
+        //assertEquals(dailyIntake.getFat(), expectedDailyIntake.getFat());
+        //assertEquals(dailyIntake.getCarbohydrate(), expectedDailyIntake.getCarbohydrate());
     }
 }
