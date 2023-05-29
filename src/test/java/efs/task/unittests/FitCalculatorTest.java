f9ac91998ac1c68c6a4e17f2d25821281ded7ae7
zywczak <123404938+zywczak@users.noreply.github.com>
2023-05-29 18:54:00 +0200

    Update FitCalculatorTest.java

--- src/test/java/efs/task/unittests/FitCalculatorTest.java
+++ src/test/java/efs/task/unittests/FitCalculatorTest.java
@@ -2,88 +2,135 @@ package efs.task.unittests;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions.assertNull;
+import org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 class FitCalculatorTest {
-    FitCalculatorTest() {
-    }
 
     @Test
     void shouldReturnTrue_whenDietRecommended() {
+        // given
         double weight = 89.2;
         double height = 1.72;
-        boolean recommended = FitCalculator.isBMICorrect(weight, height);
-        Assertions.assertTrue(recommended);
-    }
 
+        // when
+        boolean recommended = FitCalculator.isBMICorrect(weight, height);
+
+        // then
+        assertTrue(recommended);
+    }
+/*
+    @Test
+    void shouldReturnFalse_whenDietRecommended() {
+        // given
+        double weight = 69.5;
+        double height = 1.72;
+
+        // when
+        boolean recommended = FitCalculator.isBMICorrect(weight, height);
+
+        // then
+        assertFalse(recommended);
+    }
+*/
     @Test
     void shouldThrowIllegalArgumentException_whenHeightIsZero() {
-        double weight = 89.2;
+        // given
+        double weight = 80.0;
         double height = 0.0;
-        Class expectedException = IllegalArgumentException.class;
-        Assertions.assertThrows(expectedException, () -> FitCalculator.isBMICorrect(weight, height));
+
+        // then
+        assertThrows(IllegalArgumentException.class, () -> {
+            // when
+            FitCalculator.isBMICorrect(weight, height);
+        });
     }
 
-    @ParameterizedTest(
-            name = "weight: {0}"
-    )
-    @ValueSource(
-            doubles = {92.0, 102.0, 99.2}
-    )
+    @ParameterizedTest(name = "weight: {0}")
+    @ValueSource(doubles = {60.0, 70.0, 80.0})
     void shouldReturnTrue_whenDietIsRecommended(double weight) {
+        // given
         double height = 1.8;
+
+        // when
         boolean recommended = FitCalculator.isBMICorrect(weight, height);
-        Assertions.assertTrue(recommended);
+
+        // then
+        assertTrue(recommended);
     }
 
-    @ParameterizedTest(
-            name = "weight: {0}, height: {1}"
-    )
-    @CsvSource({"1.80, 72", "1.80, 63.0", "1.80, 79.2"})
+    @ParameterizedTest(name = "height: {0}, weight: {1}")
+    @CsvSource({
+            "1.70, 70.0",
+            "1.80, 80.5",
+            "1.60, 65.2"
+    })
     void shouldReturnFalse_whenDietIsNotRecommended(double height, double weight) {
+        // when
         boolean recommended = FitCalculator.isBMICorrect(weight, height);
-        Assertions.assertFalse(recommended);
+
+        // then
+        assertFalse(recommended);
     }
 
-    @ParameterizedTest(
-            name = "weight: {0}, height: {1}"
-    )
-    @CsvFileSource(
-            resources = {"/data.csv"},
-            numLinesToSkip = 1
-    )
-    void shouldReturnFalse_whenDietIsRecommendedFromFile(double weight, double height) {
+    @ParameterizedTest(name = "height: {0}, weight: {1}")
+    @CsvFileSource(resources = {"/data.csv"}, numLinesToSkip = 1)
+    void shouldReturnFalse_whenDietIsRecommendedFromCsv(double height, double weight) {
+        // when
         boolean recommended = FitCalculator.isBMICorrect(weight, height);
-        Assertions.assertFalse(recommended);
+
+        // then
+        assertFalse(recommended);
     }
 
     @Test
-    void shouldReturnUserWithTheWorstBMI() {
-        List<User> testConstants = TestConstants.TEST_USERS_LIST;
+    void shouldReturnUserWithWorstBMI() {
+        // given
+        List<User> userList = TestConstants.TEST_USERS_LIST;
         double weight = 97.3;
         double height = 1.79;
-        User userWithTheWorstBMI = FitCalculator.findUserWithTheWorstBMI(testConstants);
-        Assertions.assertEquals(weight, userWithTheWorstBMI.getWeight());
-        Assertions.assertEquals(height, userWithTheWorstBMI.getHeight());
+        
+        // when
+        User userWithWorstBMI = FitCalculator.findUserWithTheWorstBMI(userList);
+
+        // then
+        assertEquals(weight, userWithWorstBMI.getWeight());
+        assertEquals(height, userWithWorstBMI.getHeight());
     }
 
     @Test
     void shouldReturnNull() {
-        List<User> testConstants = new ArrayList<>();
-        User userWithTheWorstBMI = FitCalculator.findUserWithTheWorstBMI(testConstants);
-        Assertions.assertNull(userWithTheWorstBMI);
+        // given
+        List<User> userList = new ArrayList<>();
+
+        // when
+        User userWithWorstBMI = FitCalculator.findUserWithTheWorstBMI(userList);
+
+        // then
+        assertNull(userWithWorstBMI);
     }
 
     @Test
-    void shouldBeEqualToGivenList() {
-        List<User> testConstants = TestConstants.TEST_USERS_LIST;
-        double[] expectedBmiScore = TestConstants.TEST_USERS_BMI_SCORE;
-        double[] bmiScore = FitCalculator.calculateBMIScore(testConstants);
-        Assertions.assertArrayEquals(expectedBmiScore, bmiScore);
+    void shouldReturnEqualToList() {
+        // given
+        List<User> userList = TestConstants.TEST_USERS_LIST;
+
+        // when
+       double[] bmiScores = FitCalculator.calculateBMIScore(userList);
+
+        // then
+        double[] expectedBmiScores = TestConstants.TEST_USERS_BMI_SCORE;
+        assertArrayEquals(expectedBmiScores, bmiScores);
     }
 }
