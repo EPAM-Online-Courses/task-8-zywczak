66a42bea9eda5f906044ec27314fd1c8f74ba8a2
zywczak <123404938+zywczak@users.noreply.github.com>
2023-05-29 18:20:16 +0200

    Update PlannerTest.java

--- src/test/java/efs/task/unittests/PlannerTest.java
+++ src/test/java/efs/task/unittests/PlannerTest.java
@@ -2,7 +2,7 @@ package efs.task.unittests;
 
 import java.util.Map;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
